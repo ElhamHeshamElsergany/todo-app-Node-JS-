682bd2a9ebb135b45eee5da584ce868335b82601
ElhamHeshamElsergany <elhamhesham18@gmail.com>
2021-12-26 22:09:36 +0200

    updated patch

--- routes/users.js
+++ routes/users.js
@@ -23,14 +23,19 @@ router.post('/login', async (req, res, next) => {
   const token = await login({ username, password });
   res.json({ token })
 })
-router.patch('/:id', async (req, res, next) => {
-    const { id } = req.params;
-   await update(id, req.body).then(data => res.send('Done Edit'))
+
+router.patch('/:id', async (req, res) => {
+  const updateObject = req.body;
+  const { id } = req.params.id;
+  console.log(id, "id");
+  console.log(id, req.body, "body")
+  await User.update(req.body).then(data => res.send('Done Edit'))
     .catch(e => next(e))
 })
+
 router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-   await deleteOne(id, req.body).then(data => res.send('this todo is Deleted'))
+  const { id } = req.params;
+  await deleteOne(id, req.body).then(data => res.send('this todo is Deleted'))
     .catch(d => next(d))
 })
 module.exports = router;
